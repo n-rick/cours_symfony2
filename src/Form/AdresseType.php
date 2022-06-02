af2f2bcee153a5aabf38adf3d44015a26386c237
n-rick <e.ficadiere@gmail.com>
2022-06-02 10:56:50 +0200

    chore: 02|06 cours => add addresse and form

--- src/Form/AdresseType.php
+++ src/Form/AdresseType.php
@@ -2,28 +2,27 @@
 
 namespace App\Form;
 
-use App\Entity\Personne;
+use App\Entity\Adresse;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class PersonneType extends AbstractType
+class AdresseType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom', TextType::class, array('required' => true))
-            ->add('prenom', TextType::class)
-            ->add('adresse', AdresseType::class)
-            ->add('save', SubmitType::class, ['label' => 'Ajouter une personne']);
+        ->add('rue', TextType::class)
+        ->add('codePostal', TextType::class)
+        ->add('ville', TextType::class)
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Personne::class,
+            'data_class' => Adresse::class,
         ]);
     }
 }
