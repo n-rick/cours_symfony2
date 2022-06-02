25da4abb035964c10c42d147672e9b6f8226c5af
n-rick <e.ficadiere@gmail.com>
2022-06-02 14:27:18 +0200

    chore: 02|06 cours => cours form champs non-mappe

--- src/Form/PersonneType.php
+++ src/Form/PersonneType.php
@@ -4,16 +4,20 @@ namespace App\Form;
 
 use App\Entity\Sport;
 use App\Entity\Personne;
-use App\Repository\SportRepository;
+use App\Form\AdresseType;
 use Doctrine\ORM\EntityRepository;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Repository\SportRepository;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\FormEvent;
 
 class PersonneType extends AbstractType
 {
@@ -23,14 +27,22 @@ class PersonneType extends AbstractType
             ->add('nom', TextType::class, array('required' => true))
             ->add('prenom', TextType::class)
             ->add('adresse', AdresseType::class)
-            ->add('sports', ChoiceType::class, [
-                'choices' => [
-                    'Foot' => 'Foot',
-                    'Tennis' => 'Tennis',
-                    'Other' => null,
-                ],
-
+            ->add('sports', EntityType::class, [
+                'class' => Sport::class,
+                'choice_label' => 'name',
+                'query_builder' => function (EntityRepository $repo) {
+                    return $repo->createQueryBuilder('s');
+                },
+                'label' => 'Sports préférés',
+                'multiple' => true
             ])
+            ->add('accepter', CheckboxType::class, ['mapped' => false])
+            ->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) {
+                $personne = $event->getData();
+                if (!isset($personne['accepter']) || !$personne['accepter']) {
+                    exit;
+                }
+            })
             ->add('save', SubmitType::class, ['label' => 'Ajouter une personne']);
     }
 
